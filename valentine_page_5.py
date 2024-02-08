2b056580990eefcc227c1d581858a28e8561070d
Yash Burman <77147492+yashburman@users.noreply.github.com>
2024-02-09 02:48:43 +0530

    Update valentine_page_5.py

--- valentine_page_5.py
+++ valentine_page_5.py
@@ -18,15 +18,18 @@ image_path = "img5.jpeg"  # Replace with the actual path to your image
 col1.image(image_path, use_column_width=True, caption="Our nose looks soo cute")
 
 col2.write("""
-This day was so intense and passionate, a celebration of our love where we embraced each other's desires. We did everything we wanted , touching, talking, cuddling, tying, and getting tied :smirk:.
+This day was so intense and passionate!!! We did everything we wanted , touching, talking, cuddling, tying, and getting tied :smirk:.
 
-We danced to your favorite songs, and you wore my T-shirt. You fit perfectly into my arms, like we were meant to be together. When I move my hand to the side of your face and kiss you, it just feels so right.
+We danced to your favorite songs, and you wore my T-shirt. You fit perfectly into my arms, like we were meant to be together. When I move my hand to the side of your face and kiss you, it just feels so right. Everything we do!
+They we we touch, the way we kiss, they way we move, everything!!! And i can't keep my hands off you...i wanna give you all the massages in the world and i will take up every oppurtunity to grab and spank that very delicious booty of yours :triumph:
 
-Your lips are all I want to taste, and your eyes are all I want to look at forever.
+The point is your lips are all I want to taste, and your eyes are all I want to look at forever.
 
 Love,
 
 Your lover
+
+Yes yes onek cringe I know, move on :triumph:
 """)
 
 st.header("[Will you be mine and my...](https://mdg6rteskwzpdwrgtzzg87.streamlit.app/)")
