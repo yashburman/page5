b076cdf83733500f7a40b25146d26da48ec3da3f
Yash Burman <77147492+yashburman@users.noreply.github.com>
2024-02-09 22:59:17 +0530

    Update valentine_page_5.py

--- valentine_page_5.py
+++ valentine_page_5.py
@@ -21,7 +21,7 @@ col2.write("""
 This day was so intense and passionate!!! We did everything we wanted , touching, talking, cuddling, tying, and getting tied :smirk:.
 
 We danced to your favorite songs, and you wore my T-shirt. You fit perfectly into my arms, like we were meant to be together. When I move my hand to the side of your face and kiss you, it just feels so right. Everything we do!
-They we we touch, the way we kiss, they way we move, everything!!! And i can't keep my hands off you...i wanna give you all the massages in the world and i will take up every oppurtunity to grab and spank that very delicious booty of yours :triumph:
+They we we touch, the way we kiss, the way we move, everything!!! And i can't keep my hands off you...I wanna give you all the massages in the world and I will take up every oppurtunity to grab and spank that very delicious booty of yours :triumph:
 
 The point is ...your lips are all I want to taste, and your eyes are all I want to look at forever.
 
